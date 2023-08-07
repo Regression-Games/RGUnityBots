d306ef41b2be0593eef71ea0398517177006cefd
RG-nAmKcAz <112959031+RG-nAmKcAz@users.noreply.github.com>
2023-08-07 15:58:26 -0400

    [REG-1183] Cleanup RGCertOnlyPublicKey to not log off main thread (#15)

--- Runtime/Scripts/RGCertificateHolder.cs
+++ Runtime/Scripts/RGCertificateHolder.cs
@@ -3,29 +3,39 @@
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine.Networking;
 using System.IO;
+using JetBrains.Annotations;
 
 namespace RegressionGames
 {
     // tls/ssl seems to not be working correctly on some versions, so use this instead for now.
     class RGCertOnlyPublicKey : CertificateHandler
     {
-        private readonly static X509Certificate2 RG_CERT;
+        private X509Certificate2 RG_CERT;
 
-        static RGCertOnlyPublicKey()
+        [CanBeNull] private static RGCertOnlyPublicKey _this = null;
+        
+        public static RGCertOnlyPublicKey GetInstance()
         {
-            if (RG_CERT == null) {
-                var certFile = Path.GetFullPath("Packages/gg.regression.unity.bots/Runtime/Resources/regression_cert.cer");
-                RGDebug.LogVerbose("RG Cert loaded"+certFile);
-                RG_CERT = new X509Certificate2(certFile);
+            if (_this == null)
+            {
+                _this = new RGCertOnlyPublicKey();
             }
+
+            return _this;
+        }
+
+        private RGCertOnlyPublicKey()
+        {
+            var certFile = Path.GetFullPath("Packages/gg.regression.unity.bots/Runtime/Resources/regression_cert.cer");
+            RG_CERT = new X509Certificate2(certFile);
         }
 
         protected override bool ValidateCertificate(byte[] certificateData)
         {
             X509Certificate2 certificate = new X509Certificate2(certificateData);
-            string pk = certificate.GetPublicKeyString();
-            RGDebug.LogVerbose("Comparing Cert: " + certificate.ToString());
-            return pk.Equals(RG_CERT.GetPublicKeyString());
+            string pk = RG_CERT.GetPublicKeyString();
+            string ck = certificate.GetPublicKeyString();
+            return pk.Equals(ck);
         }
     }
 }
