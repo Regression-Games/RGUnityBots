99107fbdf3afe5da606a47c938d2640ed2e0fb89
vontell <vontell@mit.edu>
2023-08-22 15:46:51 -0400

    Add command line args to env var approach

--- Runtime/Scripts/RGEnvVars.cs
+++ Runtime/Scripts/RGEnvVars.cs
@@ -1,13 +1,65 @@
+using System;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+
 namespace RegressionGames
 {
     
     /**
-     * Environment variable constants for use in both our SDK and in user's testing
+     * Environment variable constants and utilities for configuring Regression Games
      */
     public class RGEnvVars
     {
         public static readonly string RG_API_KEY = "RG_API_KEY";
         public static readonly string RG_HOST = "RG_HOST";
         public static readonly string RG_BOT = "RG_BOT";
+
+        /**
+         * Reads the RG_API_KEY from either the environment variable or command
+         * line arguments. This is useful for automated CI builds, where the
+         * values may be passed in as command line args and not as env vars
+         * necessarily.
+         */
+        [CanBeNull]
+        public static string ReadAPIKey() => ReadEnvVarOrCommandLine(RG_API_KEY);
+        
+        /**
+         * Reads the RG_HOST from either the environment variable or command
+         * line arguments. This is useful for automated CI builds, where the
+         * values may be passed in as command line args and not as env vars
+         * necessarily.
+         */
+        [CanBeNull]
+        public static string ReadHost() => ReadEnvVarOrCommandLine(RG_HOST);
+        
+        /**
+         * Reads the BOT from either the environment variable or command
+         * line arguments. This is useful for automated CI builds, where the
+         * values may be passed in as command line args and not as env vars
+         * necessarily.
+         */
+        [CanBeNull]
+        public static string ReadBotId() => ReadEnvVarOrCommandLine(RG_BOT);
+
+        [CanBeNull]
+        private static string ReadEnvVarOrCommandLine(string key)
+        {
+            // First, try to read the env var
+            var value = Environment.GetEnvironmentVariable(key);
+            if (value != null)
+            {
+                return value;
+            }
+            // Otherwise, read the command line args
+            var commandLineArgs = Environment.GetCommandLineArgs();
+            var index = Array.FindIndex(commandLineArgs, s => s.Equals("-" + key)) + 1;
+            if (index == 0 || index >= commandLineArgs.Length)
+            {
+                return null;
+            }
+            return commandLineArgs[index];
+        }
+
     }
 }
