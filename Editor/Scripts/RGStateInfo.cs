4c00549019d75954881271571a36b6598cac6018
rcornwall-reg <139159807+rcornwall-reg@users.noreply.github.com>
2023-09-20 12:56:53 -0700

    [REG-1232] Prototype, design, and implement a new RGState approach that uses attributes (#37)

---
+++ Editor/Scripts/RGStateInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RegressionGames
+{
+    [System.Serializable]
+    public class RGStateInfo
+    {
+        public string FieldType;
+        public string FieldName;
+        public string StateName;
+        public string Type;
+    }
+}
