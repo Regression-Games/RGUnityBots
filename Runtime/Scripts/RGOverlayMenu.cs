95718b345cfff6228104abea74a17b2f11c7d50a
RG-nAmKcAz <zack@regression.gg>
2023-10-05 09:39:19 -0400

    [REG-1298] Handle null gameEngine on bot records

--- Runtime/Scripts/RGOverlayMenu.cs
+++ Runtime/Scripts/RGOverlayMenu.cs
@@ -213,7 +213,7 @@ namespace RegressionGames
                     ConcurrentBag<RGBotInstance> instances = new ConcurrentBag<RGBotInstance>();
                     foreach (RGBot bot in bots)
                     {
-                        if (bot.gameEngine.Equals("UNITY"))
+                        if (bot.gameEngine == "UNITY")
                         {
                             dropOptions.Add(new TMP_Dropdown.OptionData($"{bot.id} - {bot.name}"));
                             _ = rgServiceManager.GetRunningInstancesForBot(
