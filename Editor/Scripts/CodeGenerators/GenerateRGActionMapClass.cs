f1c248ac057ea73c07c7ef48ac078330707d2a53
RG-nAmKcAz <zack@regression.gg>
2023-10-17 10:51:45 -0400

    [REG-1302] Fix no namespace generation issue

--- Editor/Scripts/CodeGenerators/GenerateRGActionMapClass.cs
+++ Editor/Scripts/CodeGenerators/GenerateRGActionMapClass.cs
@@ -19,9 +19,12 @@ namespace RegressionGames
             List<UsingDirectiveSyntax> usings = new() {SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("UnityEngine"))};
             foreach (var rgActionInfo in botActions)
             {
-                usings.Add(
-                    SyntaxFactory.UsingDirective(SyntaxFactory.ParseName($"{rgActionInfo.Namespace}"))
-                );
+                if (rgActionInfo.Namespace != null)
+                {
+                    usings.Add(
+                        SyntaxFactory.UsingDirective(SyntaxFactory.ParseName($"{rgActionInfo.Namespace}"))
+                    );
+                }
             }
 
             // Create a namespace and class declaration
--- Editor/Scripts/CodeGenerators/RGActionCodeGenerator.cs
+++ Editor/Scripts/CodeGenerators/RGActionCodeGenerator.cs
@@ -72,7 +72,8 @@ namespace RegressionGames
 
                 foreach (var method in botActionMethods)
                 {
-                    string nameSpace = method.Ancestors().OfType<NamespaceDeclarationSyntax>().First().Name.ToString();
+                    var namespaceAncestor = method.Ancestors().OfType<NamespaceDeclarationSyntax>().FirstOrDefault(null);
+                    string nameSpace = namespaceAncestor?.Name.ToString();
                     string className = method.Ancestors().OfType<ClassDeclarationSyntax>().First().Identifier.ValueText;
                     string methodName = method.Identifier.ValueText;
 
