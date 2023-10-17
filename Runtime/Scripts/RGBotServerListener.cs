2fe7e3b68ab27a63e4d166d36adc6c869dae6505
abeizer <abby@regression.gg>
2023-10-17 15:53:28 -0400

    [REG-1336] allow multiple state scripts per GameObject

--- Runtime/Scripts/RGBotServerListener.cs
+++ Runtime/Scripts/RGBotServerListener.cs
@@ -498,6 +498,8 @@ namespace RegressionGames
             }
         }
 
+        
+        
         /**
          * Gets the entire game state by searching for all RGState game objects and gather their
          * states.
@@ -507,49 +509,47 @@ namespace RegressionGames
             var overlayAgent = this.gameObject.GetComponent<RGEntity>();
 
             var statefulObjects = FindObjectsOfType<MonoBehaviour>(true).OfType<IRGState>();
-            var totalState = new Dictionary<string, RGStateEntity>();
+            var fullGameState = new Dictionary<string, RGStateEntity>();
             
             // SADLY... Unity's threading model sucks and accessing the transform of an object must be done on the main thread only
             // thus, this code cannot really be run in parallel, causing a major object count scaling issue....
-            foreach (var rgState in statefulObjects)
+            foreach (var statefulObject in statefulObjects)
             {
-                var state = rgState.GetGameObjectState();
-
-                bool? isPlayer = (bool?)state.GetField("isPlayer");
-
-                // if this object is a 'player' ... put the clientId that owns it into the state
-                if (true.Equals(isPlayer))
+                var gameObjectState = statefulObject.GetGameObjectState();
+                
+                var rgState = statefulObject as RGState;
+                if (rgState != null)
                 {
-                    var statefulObject = rgState as RGState;
-                    if (statefulObject != null)
+                    var rgEntity = rgState.GetComponentInParent<RGEntity>();
+                    if (rgEntity != null)
                     {
-                        var rgEntity = statefulObject.GetComponentInParent<RGEntity>();
-                        if (rgEntity != null)
+                        // inherit these state variables from RGEntity
+                        gameObjectState["isPlayer"] = rgEntity.isPlayer;
+                        gameObjectState["isRuntimeObject"] = rgEntity.isRuntimeObject;
+                        
+                        if (true.Equals(gameObjectState.isPlayer))
                         {
                             var clientId = rgEntity.ClientId;
                             if (clientId != null)
                             {
-                                state["clientId"] = clientId;
+                                gameObjectState["clientId"] = clientId;
                             }
-                        }
-
-                        if (!state.ContainsKey("clientId"))
-                        {
-                            // for things like menu bots that end up spawning a human player
-                            // use the agent from the overlay
-                            // Note: We have to be very careful here or we'll set this up wrong
-                            // we only want to give the overlay agent to the human player.
-                            // Before the clientIds are all connected, this can mess-up
-                            var clientId = agentMap.FirstOrDefault(x => x.Value.Contains(overlayAgent)).Key;
-                            if (clientId != null)
+                            
+                            if (!gameObjectState.ContainsKey("clientId"))
                             {
-                                state["clientId"] = clientId;
-                                // add the agent from the player's object to the agentMap now that 
-                                // we have detected that they are here 
-                                // this happens for menu bots that spawn human players to control
-                                // doing this allows actions from the bot code to process to the human player agent
-                                if (rgEntity != null)
+                                // for things like menu bots that end up spawning a human player
+                                // use the agent from the overlay
+                                // Note: We have to be very careful here or we'll set this up wrong
+                                // we only want to give the overlay agent to the human player.
+                                // Before the clientIds are all connected, this can mess-up
+                                clientId = agentMap.FirstOrDefault(x => x.Value.Contains(overlayAgent)).Key;
+                                if (clientId != null)
                                 {
+                                    gameObjectState["clientId"] = clientId;
+                                    // add the agent from the player's object to the agentMap now that 
+                                    // we have detected that they are here 
+                                    // this happens for menu bots that spawn human players to control
+                                    // doing this allows actions from the bot code to process to the human player agent
                                     // set this to avoid expensive lookups next time
                                     rgEntity.ClientId = clientId;
                                     agentMap[clientId].Add(rgEntity);
@@ -558,9 +558,20 @@ namespace RegressionGames
                         }
                     }
                 }
-                totalState[state["id"].ToString()] = state;
+                
+                var key = gameObjectState.id.ToString();
+                if (fullGameState.TryGetValue(key, out var combinedGameObjectState))
+                {
+                    // if GameObject has multiple state scripts attached to it, then combine fields into one RGStateEntity
+                    gameObjectState.ToList().ForEach(x => combinedGameObjectState[x.Key] = x.Value);
+                    fullGameState[key] = combinedGameObjectState;
+                }
+                else
+                {
+                    fullGameState[key] = gameObjectState;
+                }
             }
-            return totalState;
+            return fullGameState;
         }
 
         public void SpawnBots(bool lateJoin = false)
