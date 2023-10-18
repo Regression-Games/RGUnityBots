747b272f14b5fa1b138b0b61163c5453fd47ad53
RG-nAmKcAz <zack@regression.gg>
2023-10-18 17:10:09 -0400

    Safe tiles in camera space in the state

--- Runtime/Scripts/RGBotConfigs/RGStateProviders/RGStateEntityPlatformer2D.cs
+++ Runtime/Scripts/RGBotConfigs/RGStateProviders/RGStateEntityPlatformer2D.cs
@@ -1,24 +1,15 @@
 using System;
 using System.Collections.Generic;
+using RegressionGames.StateActionTypes;
+using UnityEngine;
 
 namespace RegressionGames.RGBotConfigs
 {
+    // ReSharper disable InconsistentNaming
     [Serializable]
     public class RGStateEntityPlatformer2D
     {
-        public float spriteWidth = 1f;
-        
-        public List<RGStateEntity2DRaycastInfo> leftdown = new();
-        public List<RGStateEntity2DRaycastInfo> leftup = new();
-        public List<RGStateEntity2DRaycastInfo> rightdown = new();
-        public List<RGStateEntity2DRaycastInfo> rightup = new();
-        public RGStateEntity2DRaycastInfo down = new();
-        public RGStateEntity2DRaycastInfo up = new();
-        public List<RGStateEntity2DRaycastInfo> upleft = new();
-        public List<RGStateEntity2DRaycastInfo> upright = new();
-        public List<RGStateEntity2DRaycastInfo> downleft = new();
-        public List<RGStateEntity2DRaycastInfo> downright = new();
-        public RGStateEntity2DRaycastInfo left = new();
-        public RGStateEntity2DRaycastInfo right = new();
+        public Vector3 spriteSize = Vector3.one;
+        public Vector2[] platformPositions = Array.Empty<Vector2>();
     }
 }
--- Runtime/Scripts/RGBotConfigs/RGStateProviders/RGStatePlatformer2D.cs
+++ Runtime/Scripts/RGBotConfigs/RGStateProviders/RGStatePlatformer2D.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace RegressionGames.RGBotConfigs
 {
@@ -10,151 +12,101 @@ namespace RegressionGames.RGBotConfigs
      * GameObject to which this behavior is attached
      */
     [DisallowMultipleComponent]
+    [Tooltip("Provides state information about the tile grid in the current visible screen space.")]
     public class RGStatePlatformer2D: RGState
     {
-        [Header("Platformer 2D")]
+        [Tooltip("Draw debug gizmos for platform locations in editor runtime ?")]
+        public bool renderDebugGizmos = true;
         
-        [Tooltip("Render visualizations of the detection rays while the game is running in the editor (Requires Gizmos enabled).")]
-        public bool debugVisualizations = true;
-
-        [Header("Platformer 2D Raycasts")]
-        
-        [Tooltip("Offset from transform.position to center raycasts")]
-        public Vector2 offset = Vector2.zero;
-        
-        [Min(1)]
-        [Tooltip("Number of rays to check on each side up and down.")]
-        public int rayStepCount = 3;
-
-        [Tooltip("The width of the sprite grid.  Used to determine ray distance stepping")]
-        public float spriteWidth = 1f;
-
-        //[Tooltip("Distance to cast each ray. Computed based on rayStepCount and spriteWidth.")]
-        private float rayDistance = 3f;
-        
-        // all layers by default
-        [Tooltip("Filter to detect Colliders only on certain layers.")]
-        public LayerMask layerMask = Physics.DefaultRaycastLayers;
-
-        [Tooltip("Only include objects with a Z coordinate (depth) greater than or equal to this value.")]
-        public float minZDepth = -Mathf.Infinity;
-
-        [Tooltip("Only include objects with a Z coordinate (depth) less than or equal to this value.")]
-        public float maxZDepth = Mathf.Infinity;
-
-        private List<List<RGStateEntity2DRaycastInfo>> _previousRays = new();
-
-        private void Start()
+        private void OnDrawGizmos()
         {
-            rayDistance = rayStepCount * spriteWidth;
+            if (renderDebugGizmos)
+            {
+                DrawDebugPositions(lastPositions, lastCellSize);
+            }
         }
+
+        private List<Vector2> lastPositions = new();
+        private Vector3 lastCellSize = Vector3.one;
 
         protected override Dictionary<string, object> GetState()
         {
-            RGStateEntityPlatformer2D platformer2D = new()
-            {
-                spriteWidth = spriteWidth
-            };
+            var tileMap = gameObject.GetComponent<Tilemap>();
 
-            List<RGStateEntity2DRaycastInfo> centerList = new();
-            for (var i = 1; i <= rayStepCount; i++)
-            {
-                evaluateRay(i, Vector2.left, Vector2.down, platformer2D.leftdown);
-                evaluateRay(i, Vector2.left, Vector2.up, platformer2D.leftup);
-                evaluateRay(i, Vector2.right, Vector2.down, platformer2D.rightdown);
-                evaluateRay(i, Vector2.right,Vector2.up, platformer2D.rightup);
-            }
-            
-            platformer2D.down = evaluateRay(0, Vector2.right,Vector2.down, centerList);
-            platformer2D.up = evaluateRay(0, Vector2.right,Vector2.up, centerList);
-            
-            for (var i = 1; i <= rayStepCount; i++)
-            {
-                evaluateRay(i, Vector2.up, Vector2.left, platformer2D.upleft);
-                evaluateRay(i, Vector2.up, Vector2.right, platformer2D.upright);
-                evaluateRay(i, Vector2.down, Vector2.left, platformer2D.downleft);
-                evaluateRay(i, Vector2.down,Vector2.right, platformer2D.downright);
-            }
-            
-            platformer2D.left = evaluateRay(0, Vector2.up,Vector2.left, centerList);
-            platformer2D.right = evaluateRay(0, Vector2.up,Vector2.right, centerList);
+            var mainCamera = Camera.main;
 
-            _previousRays = new List<List<RGStateEntity2DRaycastInfo>>
+            var cellBounds = tileMap.cellBounds;
+            
+            if (mainCamera != null)
             {
-                platformer2D.leftdown,
-                platformer2D.leftup,
-                platformer2D.rightdown,
-                platformer2D.rightup,
+                var screenHeight = mainCamera.pixelHeight;
+                var screenWidth = mainCamera.pixelWidth;
+
+                var bottomLeft = mainCamera.ScreenToWorldPoint(Vector3.zero);
+                var topRight = mainCamera.ScreenToWorldPoint(new Vector3(screenWidth, screenHeight, 0));
+
+                var tileBottomLeft = tileMap.WorldToCell(bottomLeft);
+                tileBottomLeft.z = cellBounds.zMin;
+                var tileTopRight = tileMap.WorldToCell(topRight);
+                tileTopRight.z = cellBounds.zMax;
                 
-                platformer2D.upleft,
-                platformer2D.upright,
-                platformer2D.downleft,
-                platformer2D.downright,
-
-                centerList
-            };
-
-            return new Dictionary<string, object>
-            {
-                { "platformer2D", platformer2D}
-            };
-        }
-
-        private RGStateEntity2DRaycastInfo evaluateRay(int index, Vector2 offsetDirection, Vector2 direction, List<RGStateEntity2DRaycastInfo> raycastHits)
-        {
-            var startPosition = transform.position + (Vector3)offset + (Vector3)(offsetDirection * (index * spriteWidth));
-            RaycastHit2D raycastHit = Physics2D.Raycast(
-                startPosition,
-                direction,
-                rayDistance,
-                layerMask,
-                minZDepth,
-                maxZDepth);
-
-            string objectType = null;
-            int? objectId = null;
-            if (raycastHit.collider != null)
-            {
-                var entity = raycastHit.collider.gameObject.GetComponent<RGEntity>();
-                if (entity != null)
-                {
-                    objectType = entity.objectType;
-                    objectId = entity.transform.GetInstanceID();
-                }
+                
+                cellBounds = new BoundsInt(tileBottomLeft, tileTopRight - tileBottomLeft);
             }
+            
+            
 
-            var result = new RGStateEntity2DRaycastInfo()
+            var safePositions = new List<Vector2>();
+            for (int x = cellBounds.xMin; x < cellBounds.xMax; x++)
             {
-                from = startPosition,
-                direction = direction,
-                hitCollider = raycastHit.collider,
-                hitPoint = raycastHit.collider != null ? raycastHit.point : null,
-                hitObjectId = objectId,
-                hitObjectType = objectType
-            };
-            raycastHits.Add(result);
-            return result;
-        }
-        
-        private void Update()
-        {
-            if (debugVisualizations)
-            {
-                if (_previousRays.Count > 0)
+                for (int y = cellBounds.yMin; y < cellBounds.yMax; y++)
                 {
-                    foreach (var raycasts in _previousRays)
+                    for (int z = cellBounds.zMin; z <= cellBounds.zMax; z++)
                     {
-                        foreach (var ray in raycasts)
+                        // checks for colliders to avoid decoration sprites being considered
+                        var cellPlace = new Vector3Int(x, y, z);
+                        var tile = tileMap.GetTile<UnityEngine.Tilemaps.Tile>(cellPlace);
+                        if (tile != null && tile.colliderType  != Tile.ColliderType.None)
                         {
-                            Vector3 end = ray.hitPoint ?? (ray.from + ray.direction * rayDistance);
-                            // NOTE: Requires Gizmos to be enabled in the editor
-                            Debug.DrawLine(ray.from, end, (ray.hitPoint == null ? Color.white : Color.red), 0.0f,
-                                false);
+                            var cellPlaceAbove = cellPlace + Vector3Int.up;
+                            var tileAbove = tileMap.GetTile<UnityEngine.Tilemaps.Tile>(cellPlaceAbove);
+                            //Tile at "place", see if there is a tile above it or not before adding
+                            if (tileAbove == null || tileAbove.colliderType == Tile.ColliderType.None)
+                            {
+                                Vector3 place = tileMap.CellToWorld(cellPlaceAbove);
+                                safePositions.Add(place);
+                            }
+                        }
+                        else
+                        {
+                            //No tile at "place"
                         }
                     }
                 }
             }
+            
+            lastPositions = safePositions;
+            lastCellSize = tileMap.cellSize;
+
+            return new Dictionary<string, object>()
+            {
+                {
+                    "platformer2D", new RGStateEntityPlatformer2D()
+                    {
+                        spriteSize = lastCellSize,
+                        platformPositions = safePositions.ToArray()
+                    }
+                }
+            };
+        }
+
+        private void DrawDebugPositions(List<Vector2> platformPositions, Vector3 cellSize)
+        {
+            foreach (var platformPosition in platformPositions)
+            {
+                Gizmos.DrawWireSphere(new Vector3(platformPosition.x + cellSize.x / 2, platformPosition.y + cellSize.y / 2, 0),
+                    cellSize.x/2);
+            }
         }
     }
-
 }
