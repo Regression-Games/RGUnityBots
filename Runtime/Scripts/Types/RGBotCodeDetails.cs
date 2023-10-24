92da440d12d301a7da177ccb2bcbc3337e89cc9a
RG-nAmKcAz <zack@regression.gg>
2023-10-24 13:20:38 -0400

    [REG-1306] Working synchronization

---
+++ Runtime/Scripts/Types/RGBotCodeDetails.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RegressionGames.Types
+{
+    [Serializable]
+    public class RGBotCodeDetails
+    {
+        // ReSharper disable InconsistentNaming
+        public long botId;
+        public long fileSize;
+        public string md5;
+        // ReSharper enable InconsistentNaming
+    }
+}
