e30d4a00cb90fba32752feacee6fc655c16619bb
RG-nAmKcAz <112959031+RG-nAmKcAz@users.noreply.github.com>
2023-10-25 12:56:41 -0400

    [REG-1306] Synchronize Unity Bots With RG Server (#72)

--- Runtime/Scripts/Types/RGClientConnection_Remote.cs
+++ Runtime/Scripts/Types/RGClientConnection_Remote.cs
@@ -22,7 +22,7 @@ namespace RegressionGames.Types
 
         private const int SOCKET_READWRITE_TIMEOUT = 5_000; // 5 seconds
 
-        private SemaphoreSlim _connecting = new (0,1);
+        private SemaphoreSlim _connecting = new (1,1);
         [CanBeNull] private RGBotInstanceExternalConnectionInfo _connectionInfo;
 
         public RGClientConnection_Remote(long clientId, string lifecycle = "MANAGED",
