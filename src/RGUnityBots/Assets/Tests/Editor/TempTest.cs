5f3afe4d9268e81d966276da5c1ff6f5269dc299
Ashley Stanton-Nurse <git@analogrelay.net>
2023-12-08 15:18:37 -0800

    add RGUnityBots host project and build scripts

---
+++ src/RGUnityBots/Assets/Tests/Editor/TempTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Tests.Editor
+{
+    public class TempTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.True(false);
+        }
+    }
+}
