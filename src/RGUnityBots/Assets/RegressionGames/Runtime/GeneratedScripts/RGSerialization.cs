e57abd9347fcea6a23bb27eb06c67132a23570a6
Ashley Stanton-Nurse <github@analogrelay.net>
2023-12-11 14:30:17 -0800

    [REG-1495] Reorganize Unity projects and add CI/CD (#121)

---
+++ src/RGUnityBots/Assets/RegressionGames/Runtime/GeneratedScripts/RGSerialization.cs
@@ -0,0 +1,15 @@
+/*
+* This file has been automatically generated. Do not modify.
+*/
+
+using System;
+using Newtonsoft.Json;
+
+namespace RGRGUnityBots
+{
+    using UnityEngine;
+
+    public static class RGSerialization
+    {
+    }
+}
