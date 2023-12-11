1202b87cfcbdda8fcaca67367d2914c0a865f1ca
Ashley Stanton-Nurse <git@analogrelay.net>
2023-12-11 10:44:03 -0800

    add a build workflow

--- src/gg.regression.unity.bots/Runtime/Scripts/ActiveRGBotUIElement.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/ActiveRGBotUIElement.cs
@@ -14,6 +14,7 @@ namespace RegressionGames
 
         public void Awake()
         {
+            An Intentional Compilation Error!!!
             if (_entry != null)
             {
                 var state = RGBotServerListener.GetInstance()?.GetUnityBotState(_entry.id);
