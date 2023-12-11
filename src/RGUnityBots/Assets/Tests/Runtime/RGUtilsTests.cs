8d671eecf7e1c4b01e4688405cd7b5740750af90
Ashley Stanton-Nurse <git@analogrelay.net>
2023-12-11 12:18:14 -0800

    fix intentional errors

--- src/RGUnityBots/Assets/Tests/Runtime/RGUtilsTests.cs
+++ src/RGUnityBots/Assets/Tests/Runtime/RGUtilsTests.cs
@@ -12,7 +12,7 @@ namespace Tests.Runtime
             var tmpFile = Path.GetTempFileName();
             File.WriteAllText(tmpFile, "Hello, World!");
             var hash = RGUtils.CalculateMD5(tmpFile);
-            Assert.AreEqual("a5a8e27d8879283831b664bd8b7f0ad4", hash);
+            Assert.AreEqual("65a8e27d8879283831b664bd8b7f0ad4", hash);
         }
     }
 }
