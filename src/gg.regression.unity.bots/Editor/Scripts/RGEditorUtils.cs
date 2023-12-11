b059a8b75094ad500665716dd8d1718da6a4b1e0
Ashley Stanton-Nurse <github@analogrelay.net>
2023-12-11 14:54:37 -0800

    [REG-1488] Prompt for new bot name and decouple local bots from their IDs (#115)

---
+++ src/gg.regression.unity.bots/Editor/Scripts/RGEditorUtils.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEditor;
+
+namespace RegressionGames.Editor
+{
+    public static class RGEditorUtils
+    {
+        public static string GetAssetPathParent(string assetPath)
+        {
+            var lastSlashIndex = assetPath.LastIndexOf('/');
+            if (lastSlashIndex < 0)
+            {
+                return null;
+            }
+
+            return assetPath[..lastSlashIndex];
+        }
+
+        public static string GetAssetPathLeaf(string assetPath)
+        {
+            var lastSlashIndex = assetPath.LastIndexOf('/');
+            if (lastSlashIndex < 0)
+            {
+                return null;
+            }
+
+            return assetPath[(lastSlashIndex + 1)..];
+        }
+
+        public static void CreateAllAssetFolders(string path)
+        {
+            path = path.TrimEnd('/');
+
+            if (AssetDatabase.IsValidFolder(path))
+            {
+                // This path already exists as a folder, so we're done.
+                return;
+            }
+
+            if (path == "Assets")
+            {
+                // The asset folder should always exist, so if we get here, something is wrong.
+                throw new InvalidOperationException("Asset folder does not exist");
+            }
+
+            // We need to create this directory, but we may need to create it's parents too.
+            var parentPath = GetAssetPathParent(path);
+            var leafName = GetAssetPathLeaf(path);
+            CreateAllAssetFolders(parentPath);
+            var createdGuid = AssetDatabase.CreateFolder(parentPath, leafName);
+            var createdPath = AssetDatabase.GUIDToAssetPath(createdGuid);
+
+            // Unity will deduplicate folder names in CreateFolder if the folder already exists
+            // However, we should be OK because we've already checked that the folder doesn't exist.
+            // Still, make sure we got the folder we expected.
+            if (createdPath != path)
+            {
+                throw new InvalidOperationException("Unexpected folder name from AssetDatabase.CreateFolder");
+            }
+        }
+    }
+}
