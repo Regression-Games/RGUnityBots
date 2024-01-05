d64369aac827cbf4c046b653d5d47bdfecc786ec
RG-nAmKcAz <zack@regression.gg>
2024-01-05 14:12:41 -0500

    [REG-1525] Re-add typed findutils method

--- src/gg.regression.unity.bots/Runtime/Scripts/RGFindUtils.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/RGFindUtils.cs
@@ -17,8 +17,7 @@ namespace RegressionGames
         private readonly Dictionary<int, GameObject> _objectCache = new Dictionary<int, GameObject>();
 
         /**
-         * Finds an object by instance ID, using a type parameter for narrowing down
-         * the number of objects to search for in the scene.
+         * Finds a GameObject by instance ID
          */
         public GameObject FindOneByInstanceId(int instanceId) 
         {
@@ -29,7 +28,29 @@ namespace RegressionGames
             }
             return _objectCache.GetValueOrDefault(instanceId, null);
         }
+        
+        /**
+         * Finds a MonoBehaviour on a GameObject by instance ID, using a type parameter for
+         * specifying which MonoBehaviour on the Object to choose.
+         */
+        public T FindOneByInstanceId<T>(int instanceId) where T : MonoBehaviour
+        {
+            // If the requested object is not in the cache, refresh the cache
+            if (!_objectCache.ContainsKey(instanceId))
+            {
+                UpdateCache();
+            }
 
+            var gameObject = _objectCache.GetValueOrDefault(instanceId, null);
+            if (gameObject == null)
+            {
+                return null;
+            }
+
+            var result = gameObject.GetComponent<T>();
+            return result;
+        }
+        
         private void UpdateCache()
         {
             var gameObjects = FindStatefulAndActionableBehaviours().Select(v => v.gameObject).Distinct();
