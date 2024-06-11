ee35a7d093635cb9c09deb36fd5f45b3b6ce0f5b
Sasha Volokh <61521182+svolokh@users.noreply.github.com>
2024-06-11 10:04:22 -0700

    [REG-1776] Automated tests for legacy input support (#191)

---
+++ src/RGUnityBots/Assets/Scripts/LegacyInputTests/LegacyAxisHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LegacyAxisHandler : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0.0f)
+        {
+            Debug.Log("GetAxis(\"Horizontal\") > 0.0f");
+        } else if (Input.GetAxis("Horizontal") < 0.0f)
+        {
+            Debug.Log("GetAxis(\"Horizontal\") < 0.0f");
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.0f)
+        {
+            Debug.Log("GetAxisRaw(\"Vertical\") > 0.0f");
+        } else if (Input.GetAxisRaw("Vertical") < 0.0f)
+        {
+            Debug.Log("GetAxisRaw(\"Vertical\") < 0.0f");
+        }
+    }
+}
