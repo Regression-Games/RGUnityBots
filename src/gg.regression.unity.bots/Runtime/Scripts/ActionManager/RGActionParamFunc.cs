a96b8d2477fe655c993033215971fb6fc81963f4
Sasha Volokh <sasha@regression.gg>
2024-06-27 12:11:27 -0700

    start implementing RGActionParamFunc

---
+++ src/gg.regression.unity.bots/Runtime/Scripts/ActionManager/RGActionParamFunc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegressionGames.ActionManager
+{
+    /// <summary>
+    /// This represents a function that
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class RGActionParamFunc<T>
+    {
+        public Func<Object, T> Function { get; }
+
+        public string Identifier { get; }
+    }
+}
