dc42e2d139b369b4fffc86f50ff2875c00278aca
Sasha Volokh <sasha@regression.gg>
2024-07-05 11:10:03 -0700

    another improvement to monkey bot based on testing

--- src/gg.regression.unity.bots/Runtime/Scripts/GenericBots/RGMonkeyBot.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/GenericBots/RGMonkeyBot.cs
@@ -22,6 +22,15 @@ namespace RegressionGames.GenericBots
 
         private IList<MonkeyBotActionEntry> _actionsBuf;
         private IList<MonkeyBotActionEntry> _remainingActionsBuf;
+
+        private ISet<int> _mouseBtnsBuf;
+
+        private void ResetState()
+        {
+            _actionsBuf.Clear();
+            _remainingActionsBuf.Clear();
+            _mouseBtnsBuf.Clear();
+        }
     
         void Start()
         {
@@ -36,6 +45,7 @@ namespace RegressionGames.GenericBots
             _lastActionTime = Time.unscaledTime;
             _actionsBuf = new List<MonkeyBotActionEntry>();
             _remainingActionsBuf = new List<MonkeyBotActionEntry>();
+            _mouseBtnsBuf = new HashSet<int>();
             
             DontDestroyOnLoad(this);
         }
@@ -63,9 +73,10 @@ namespace RegressionGames.GenericBots
             }
             _lastActionTime = currentTimeUnscaled;
             
-            // If the last input sent was a press on a button that is still valid, always do a release on this frame to activate the button
-            // This heuristic makes it more likely to trigger a button press.
-            bool didReleaseBtn = false;
+            bool didHeuristic = false;
+            
+            // Heuristic: If the last action was a mouse down on a Unity UI button, always release the mouse button on this frame over
+            // the same mouse coordinate to trigger the click event
             foreach (var performedAction in _actionsBuf.Where(act => act.Performed))
             {
                 if (performedAction.ActionInstance is UIButtonPressInstance && (bool)performedAction.Parameter)
@@ -78,17 +89,61 @@ namespace RegressionGames.GenericBots
                         {
                             inp.Perform();
                         }
-                        didReleaseBtn = true;
+                        didHeuristic = true;
                     }
                 }
             }
-
-            _actionsBuf.Clear();
-            
-            if (didReleaseBtn)
+            if (didHeuristic)
             {
+                ResetState();
                 return;
             }
+            
+            // Heuristic: If the last set of inputs was a mouse position movement + a set of mouse button presses, 
+            // then have some random chance of releasing those buttons over the same mouse position to complete a potential click event.
+            // Otherwise, in general it is very unlikely that a mouse button press and release would occur over the same position.
+            {
+                bool haveMousePos = false;
+                _mouseBtnsBuf.Clear();
+                foreach (var inp in _actionsBuf.Where(act => act.Performed).SelectMany(act => act.Inputs))
+                {
+                    if (inp is MousePositionInput)
+                    {
+                        haveMousePos = true;
+                    } else if (inp is MouseButtonInput mbInput)
+                    {
+                        if (mbInput.IsPressed)
+                        {
+                            _mouseBtnsBuf.Add(mbInput.MouseButton);
+                        }
+                    } else if (inp is LegacyKeyInput keyInput)
+                    {
+                        if (keyInput.KeyCode >= KeyCode.Mouse0 && keyInput.KeyCode <= KeyCode.Mouse6)
+                        {
+                            _mouseBtnsBuf.Add(keyInput.KeyCode - KeyCode.Mouse0);
+                        }
+                    }
+                }
+
+                if (haveMousePos && _mouseBtnsBuf.Count > 0)
+                {
+                    if (Random.Range(0, 2) == 1) // 50% chance of attempting a click event
+                    {
+                        foreach (var btn in _mouseBtnsBuf)
+                        {
+                            new MouseButtonInput(btn, false).Perform();
+                        }
+                        didHeuristic = true;
+                    }
+                }
+            }
+            if (didHeuristic)
+            {
+                ResetState();
+                return;
+            }
+            
+            ResetState();
 
             // Compute the set of valid actions
             foreach (var actionInstance in RGActionManager.GetValidActions())
