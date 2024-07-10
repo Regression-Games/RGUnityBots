ee2a1154902fad0a1df6a21d77be9940acb09c00
RG-nAmKcAz <zack@regression.gg>
2024-07-10 17:04:12 -0400

    Update PerFrameStatisticsData.cs

--- src/gg.regression.unity.bots/Runtime/Scripts/StateRecorder/Models/PerFrameStatisticsData.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/StateRecorder/Models/PerFrameStatisticsData.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
-using OpenTelemetry;
 using RegressionGames.StateRecorder.JsonConverters;
 
 namespace RegressionGames.StateRecorder.Models
