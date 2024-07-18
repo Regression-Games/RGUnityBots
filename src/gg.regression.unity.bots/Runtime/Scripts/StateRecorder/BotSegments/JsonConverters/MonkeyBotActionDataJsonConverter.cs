25a32eb9081e702551c8f2c50eac953269a93a36
Sasha Volokh <61521182+svolokh@users.noreply.github.com>
2024-07-18 12:28:46 -0700

    [REG-1859] Monkey bot segment (#229)

---
+++ src/gg.regression.unity.bots/Runtime/Scripts/StateRecorder/BotSegments/JsonConverters/MonkeyBotActionDataJsonConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RegressionGames.ActionManager;
+using RegressionGames.StateRecorder.BotSegments.Models;
+
+namespace RegressionGames.StateRecorder.BotSegments.JsonConverters
+{
+    public class MonkeyBotActionDataJsonConverter : JsonConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jObject = JObject.Load(reader);
+            MonkeyBotActionData data = new MonkeyBotActionData();
+            data.apiVersion = jObject["apiVersion"].ToObject<int>();
+            data.actionInterval = jObject["actionInterval"].ToObject<float>();
+            data.actionSettings = jObject["actionSettings"].ToObject<RGActionManagerSettings>();
+            return data;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(MonkeyBotActionData).IsAssignableFrom(objectType);
+        }
+        
+        public override bool CanWrite => false;
+        
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
