897062b3492f410efdc91f0966a002e44fb2c218
Abby Beizer <abby@regression.gg>
2024-07-19 10:44:59 -0400

    update bot-instance API paths (#222)

--- src/gg.regression.unity.bots/Runtime/Scripts/Types/RGCreateBotInstanceRequest.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/Types/RGCreateBotInstanceRequest.cs
@@ -6,10 +6,12 @@ namespace RegressionGames.Types
     [Serializable]
     public class RGCreateBotInstanceRequest
     {
+        public long botId;
         public DateTime startDate;
 
-        public RGCreateBotInstanceRequest(DateTime startDate)
+        public RGCreateBotInstanceRequest(long botId, DateTime startDate)
         {
+            this.botId = botId;
             this.startDate = startDate;
         }
     }
