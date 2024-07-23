6a42ce5bde010a4936b3c30a49cc60c9da408dd2
RG-nAmKcAz <zack@regression.gg>
2024-07-23 16:45:14 -0400

    [REG-1843] CV Text Detection Criteria

---
+++ src/gg.regression.unity.bots/Runtime/Scripts/StateRecorder/BotSegments/Models/CVImageRequestData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using RegressionGames.StateRecorder.JsonConverters;
+
+namespace RegressionGames.StateRecorder.BotSegments.Models
+{
+    public class CVImageRequestData
+    {
+        /**
+         * <summary>This must be the image data as a PNG/JPG/etc encoded as that is what python is expecting.  NOT just the data bytes</summary>
+         */
+        public byte[] data;
+        public int width;
+        public int height;
+
+        public void WriteToStringBuilder(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append("{\n\"width\":");
+            IntJsonConverter.WriteToStringBuilder(stringBuilder, width);
+            stringBuilder.Append(",\n\"height\":");
+            IntJsonConverter.WriteToStringBuilder(stringBuilder, height);
+            stringBuilder.Append(",\n\"data\":");
+            StringJsonConverter.WriteToStringBuilder(stringBuilder, Convert.ToBase64String(data));
+            stringBuilder.Append("}");
+        }
+    }
+}
---
+++ src/gg.regression.unity.bots/Runtime/Scripts/StateRecorder/BotSegments/Models/CVTextResult.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RegressionGames.StateRecorder.JsonConverters;
+using UnityEngine;
+
+namespace RegressionGames.StateRecorder.BotSegments.Models
+{
+    public class CVTextResult
+    {
+        public string text;
+        public Vector2Int resolution;
+        public RectInt rect;
+
+        public string ToString()
+        {
+            StringBuilder sb = new(1000);
+            sb.Append("{\ntext\":");
+            StringJsonConverter.WriteToStringBuilder(sb, text);
+            sb.Append(",\resolution\":");
+            VectorIntJsonConverter.WriteToStringBuilder(sb, resolution);
+            sb.Append(",\"rect\":");
+            RectIntJsonConverter.WriteToStringBuilder(sb, rect);
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+    }
+}
