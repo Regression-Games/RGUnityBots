76be35ac76bdce73c2e05c018bf3f46159cdfd53
Sasha Volokh <61521182+svolokh@users.noreply.github.com>
2024-07-23 15:12:33 -0700

    [REG-1848] Improvements to address file in use errors during instrumentation (#232)

---
+++ src/gg.regression.unity.bots/Runtime/Scripts/RGLegacyInputUtility/RGAssemblyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Mono.Cecil;
+using UnityEngine;
+
+namespace RegressionGames.RGLegacyInputUtility
+{
+    public class RGAssemblyResolver : DefaultAssemblyResolver
+    {
+        public override AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+        {
+            // force in-memory storage of resolved assemblies
+            if (!parameters.InMemory)
+            {
+                parameters = new ReaderParameters()
+                {
+                    ReadingMode = parameters.ReadingMode,
+                    InMemory = true,
+                    AssemblyResolver = parameters.AssemblyResolver,
+                    MetadataResolver = parameters.MetadataResolver,
+                    MetadataImporterProvider = parameters.MetadataImporterProvider,
+                    ReflectionImporterProvider = parameters.ReflectionImporterProvider,
+                    SymbolStream = parameters.SymbolStream,
+                    SymbolReaderProvider = parameters.SymbolReaderProvider,
+                    ReadSymbols = parameters.ReadSymbols,
+                    ThrowIfSymbolsAreNotMatching = parameters.ThrowIfSymbolsAreNotMatching,
+                    ReadWrite = parameters.ReadWrite,
+                    ApplyWindowsRuntimeProjections = parameters.ApplyWindowsRuntimeProjections
+                };
+            }
+            return base.Resolve(name, parameters);
+        }
+    }
+}
