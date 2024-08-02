7357e46f37e6c13b3122d7b23cbb75b26dfa3373
Sasha Volokh <sasha@regression.gg>
2024-08-02 09:08:45 -0700

    remove unused imports

--- src/gg.regression.unity.bots/Runtime/Scripts/RGLegacyInputUtility/RGLegacyInputWrapper.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/RGLegacyInputUtility/RGLegacyInputWrapper.cs
@@ -3,10 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace RegressionGames.RGLegacyInputUtility
 {
