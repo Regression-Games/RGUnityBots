d76e3b4838799bc8ea6708d8d82abf10da0a1828
batu <batu@regression.gg>
2024-08-22 13:43:47 -0700

    Added Json Converter and copied action data.

---
+++ src/gg.regression.unity.bots/Runtime/Scripts/StateRecorder/BotSegments/JsonConverters/CVObjectDetectionMouseActionDataJsonConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RegressionGames.StateRecorder.BotSegments.Models;
+
+namespace RegressionGames.StateRecorder.BotSegments.JsonConverters
+{
+    public sealed class CVObjectDetectionMouseActionDataJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(CVObjectDetectionMouseActionData).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.Null)
+            {
+                JObject jObject = JObject.Load(reader);
+                // ReSharper disable once UseObjectOrCollectionInitializer - easier to debug when on separate lines
+                CVObjectDetectionMouseActionData actionModel = new();
+                actionModel.apiVersion = jObject.GetValue("apiVersion").ToObject<int>(serializer);
+                actionModel.imageData = jObject.GetValue("imageData").ToObject<string>(serializer);
+                actionModel.withinRect = jObject.GetValue("withinRect")?.ToObject<CVWithinRect>(serializer);
+                actionModel.actions = jObject.GetValue("actions").ToObject<List<CVMouseActionDetails>>(serializer);
+                return actionModel;
+            }
+
+            return null;
+        }
+
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
