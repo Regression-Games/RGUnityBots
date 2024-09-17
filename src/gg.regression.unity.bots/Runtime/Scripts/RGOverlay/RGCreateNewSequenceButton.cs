18bc7246eee8131167e34083acca0fbd2703ba97
addisonbgross <addisonbgross@gmail.com>
2024-09-17 14:50:50 -0600

    feat: delete unused file + a couple more tests

--- src/gg.regression.unity.bots/Runtime/Scripts/RGOverlay/RGCreateNewSequenceButton.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/RGOverlay/RGCreateNewSequenceButton.cs
@@ -10,7 +10,7 @@ namespace RegressionGames
     public class RGCreateNewSequenceButton : MonoBehaviour
     {
         public GameObject overlayContainer;
-        
+
         public void OnClick()
         {
             if (overlayContainer != null)
--- src/gg.regression.unity.bots/Runtime/Scripts/RGOverlay/RGEditSequenceData.cs
+++
@@ -1,9 +0,0 @@
-namespace RegressionGames.RGOverlay
-{
-    public struct RGEditSequenceData
-    {
-        public string name;
-        public string description;
-        public string path;
-    }   
-}
