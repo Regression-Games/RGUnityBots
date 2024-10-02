096c0823409aff4304674d2e8ae9502460d1d86a
addison <addisonbgross@gmail.com>
2024-10-02 16:36:37 -0600

    feat: sync GetInstance function with main

--- src/gg.regression.unity.bots/Runtime/Scripts/RGOverlay/RGSequenceManager.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/RGOverlay/RGSequenceManager.cs
@@ -40,6 +40,11 @@ public class RGSequenceManager : MonoBehaviour
 
     public static RGSequenceManager GetInstance()
     {
+        if (_this == null)
+        {
+            _this = FindObjectOfType<RGSequenceManager>();
+        }
+        
         return _this;
     }
 
@@ -81,7 +86,6 @@ public class RGSequenceManager : MonoBehaviour
 
     public void Awake()
     {
-        _this = this;
         DontDestroyOnLoad(gameObject);
     }
 
