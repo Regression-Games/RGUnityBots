7e5bac3f83d706f8485d351851fcce2bd91e213b
addison <addisonbgross@gmail.com>
2024-10-02 15:52:41 -0600

    feat: add some tests for the new segment entries;

---
+++ src/gg.regression.unity.bots/Tests/Scripts/RGOverlay/RGSegmentEntry.test.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using RegressionGames.StateRecorder.BotSegments.Models;
+using RegressionGames.TestFramework;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace RegressionGames.Tests.RGOverlay
+{
+    [TestFixture]
+    public class RGSegmentEntryTests
+    {
+        private GameObject _uat;
+
+        private RGSegmentEntry entry;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uat = new GameObject();
+            entry = _uat.AddComponent<RGSegmentEntry>();
+            entry.segmentName = "TestSegment";
+            entry.description = "TestDescription";
+            entry.path = "test/path";
+            entry.type = BotSequenceEntryType.Segment;
+            entry.nameComponent = RGTestUtils.CreateTMProPlaceholder();
+            entry.descriptionComponent = RGTestUtils.CreateTMProPlaceholder();
+            entry.playButton = _uat.AddComponent<Button>();
+            
+            // create tooltip child
+            var tooltip = new GameObject();
+            tooltip.AddComponent<RGTooltip>();
+            tooltip.transform.SetParent(entry.transform);
+
+            // create segment list indicator w/Image
+            var segmentListIndicator = new GameObject();
+            segmentListIndicator.AddComponent<Image>();
+            entry.segmentListIndicatorComponent = segmentListIndicator;
+            entry.segmentListIndicatorComponent.SetActive(false);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(_uat);
+        }
+
+        [Test]
+        public void Start()
+        {
+            entry.Start();
+            
+            Assert.AreEqual(entry.path, "test/path");
+            Assert.AreEqual(entry.nameComponent.text, entry.segmentName);
+            Assert.AreEqual(entry.descriptionComponent.text, entry.description);
+            Assert.IsFalse(entry.segmentListIndicatorComponent.activeSelf);
+        }
+        
+        [Test]
+        public void Start_SegmentList()
+        {
+            entry.type = BotSequenceEntryType.SegmentList;
+            entry.Start();
+            
+            Assert.IsTrue(entry.segmentListIndicatorComponent.activeSelf);
+        }
+    }
+}
