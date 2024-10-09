7445de2828d954d0c9ba655a51ec1e485c63f715
Aaron Vontell <aaron@vontech.org>
2024-10-09 11:54:30 -0400

    [REG-2064] Manually write files on OSX platforms (#329)

--- src/gg.regression.unity.bots/Runtime/Scripts/StateRecorder/ScreenRecorder.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/StateRecorder/ScreenRecorder.cs
@@ -329,8 +329,28 @@ namespace RegressionGames.StateRecorder
                 // So we can safely create this without interfering with the .Move command right below.
                 Directory.CreateDirectory(Path.GetDirectoryName(segmentResourceDirectory));
 
-                // move the directory (this also deletes the source directory)
-                Directory.Move(botSegmentsDirectoryPrefix, segmentResourceDirectory);
+                // If on a Mac, for some weird reason, .Move will fail for project paths that have spaces. We predict
+                // that downstream, something is happening where the space is not properly handled in Unix commands. 
+                // Unfortunately, we could only reproduce this once, but this change should fix it. On Mac, we instead
+                // use the File.Copy function.
+                if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer)
+                {
+                    Directory.CreateDirectory(segmentResourceDirectory); // Since we aren't moving, we need to create the directory
+                    foreach (var segmentFile in segmentFiles)
+                    {
+                        var sourcePath = botSegmentsDirectoryPrefix + "/" + segmentFile;
+                        var destinationPath = segmentResourceDirectory + "/" + segmentFile;
+                        File.Copy(sourcePath, destinationPath);
+                    }
+                    // Then delete the original directory
+                    Directory.Delete(botSegmentsDirectoryPrefix, true);
+                }
+                else
+                {
+                    // move the directory (this also deletes the source directory)
+                    Directory.Move(botSegmentsDirectoryPrefix, segmentResourceDirectory);
+                }
+                
             }
             catch (Exception ex)
             {
