6e7691e7cd93da75a44fa535ff33791c89545c1f
batu <batu@regression.gg>
2024-10-25 10:52:15 -0700

    Fixed a bug where the previous bot sequence wouldn't be cleared in the SDK.

--- src/gg.regression.unity.experimental.runespawn/Editor/Scripts/PlayModeController.cs
+++ src/gg.regression.unity.experimental.runespawn/Editor/Scripts/PlayModeController.cs
@@ -69,6 +69,9 @@ public static class PlayModeController
     {
         try
         {
+            // Ensure the previous bot sequence is cleared.
+            BotSequence.ActiveBotSequence = null;
+
             // Reset the previous bot sequence when a new request is received.
             _botSequence = null;
             SetupBotSequence(client, botSequencePath);
