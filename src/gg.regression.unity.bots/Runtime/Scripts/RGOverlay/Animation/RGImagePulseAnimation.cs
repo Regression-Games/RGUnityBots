c4b4d448f67656790a4ebc5683beaaa101fbe4ea
addison <addisonbgross@gmail.com>
2024-11-14 13:54:57 -0700

    feat: add some comments

--- src/gg.regression.unity.bots/Runtime/Scripts/RGOverlay/Animation/RGImagePulseAnimation.cs
+++ src/gg.regression.unity.bots/Runtime/Scripts/RGOverlay/Animation/RGImagePulseAnimation.cs
@@ -6,19 +6,24 @@ namespace RegressionGames
 {
     /**
      * <summary>
-     * A simple animation that can be added to any UI component that also has a CanvasGroup
+     * A simple, looping animation that can be added to any UI component that has an Image
      * </summary>
      */
     public class RGImagePulseAnimation : MonoBehaviour
     {
+        // how long should the increasing/decreasing phase of the animation last
         public float period = 1.0f;
 
-        public float maxOpacity = 1.0f;
+        // the highest alpha value to use when increasing
+        public float maxAlpha = 1.0f;
 
-        public float minOpacity = 0f;
+        // the lowest alpha value to use when decreasing
+        public float minAlpha = 0f;
 
+        // the image to apply the animation to
         public Image sourceImage;
 
+        // is the alpha value increasing or decreasing
         private bool _isIncreasing;
         
         private Color _currentColor;
@@ -27,18 +32,23 @@ namespace RegressionGames
         {
             _isIncreasing = true;
             _currentColor = sourceImage.color;
-            
+
             StartCoroutine(Pulse());
         }
 
+        /**
+         * <summary>
+         * Will oscillate the sourceImage's alpha value up and down, between the minOpacity and maxOpacity fields
+         * </summary>
+         */
         private IEnumerator Pulse()
         {
             if (_isIncreasing)
             {
-                float startAlpha = sourceImage.color.a;
+                var startAlpha = sourceImage.color.a;
                 for (float t = 0; t < period; t += Time.deltaTime)
                 {
-                    _currentColor.a = Mathf.Lerp(startAlpha, maxOpacity, t / period);
+                    _currentColor.a = Mathf.Lerp(startAlpha, maxAlpha, t / period);
                     sourceImage.color = _currentColor;
                     yield return null;
                 }
@@ -46,10 +56,10 @@ namespace RegressionGames
             }
             else
             {
-                float startAlpha = sourceImage.color.a;
+                var startAlpha = sourceImage.color.a;
                 for (float t = 0; t < period; t += Time.deltaTime)
                 {
-                    _currentColor.a = Mathf.Lerp(startAlpha, minOpacity, t / period);
+                    _currentColor.a = Mathf.Lerp(startAlpha, minAlpha, t / period);
                     sourceImage.color = _currentColor;
                     yield return null;
                 }
